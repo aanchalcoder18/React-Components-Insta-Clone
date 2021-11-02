624cc689eafda5f7892d66e7725f9761eec99f58
aanchalcoder18 <aasaxena14@gmail.com>
2021-11-02 13:18:30 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import React from 'react';
 // Import the dummyData
 import './App.css';
 
-const App = () => {
+const App = () => { 
+  
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
